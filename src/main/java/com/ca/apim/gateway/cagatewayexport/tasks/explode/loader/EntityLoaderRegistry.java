be1c6504994a9fa8ed06661dbb24ae0c3529d73b
Victor Kazakov <kazvictor@gmail.com>
2018-05-23 17:43:36 -0700

    removing java 9 Map.of

--- src/main/java/com/ca/apim/gateway/cagatewayexport/tasks/explode/loader/EntityLoaderRegistry.java
+++ src/main/java/com/ca/apim/gateway/cagatewayexport/tasks/explode/loader/EntityLoaderRegistry.java
@@ -6,20 +6,21 @@
 
 package com.ca.apim.gateway.cagatewayexport.tasks.explode.loader;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class EntityLoaderRegistry {
 
     private final Map<String, EntityLoader> entityLoaders;
 
     public EntityLoaderRegistry() {
-        this.entityLoaders = Map.of(
-                "SERVICE", new ServiceLoader(),
-                "POLICY", new PolicyLoader(),
-                "POLICY_BACKED_SERVICE", new PolicyBackedServiceLoader(),
-                "FOLDER", new FolderLoader(),
-                "ENCAPSULATED_ASSERTION", new EncassLoader(),
-                "CLUSTER_PROPERTY", new ClusterPropertyLoader());
+        entityLoaders = new HashMap<>();
+        entityLoaders.put("SERVICE", new ServiceLoader());
+        entityLoaders.put("POLICY", new PolicyLoader());
+        entityLoaders.put("POLICY_BACKED_SERVICE", new PolicyBackedServiceLoader());
+        entityLoaders.put("FOLDER", new FolderLoader());
+        entityLoaders.put("ENCAPSULATED_ASSERTION", new EncassLoader());
+        entityLoaders.put("CLUSTER_PROPERTY", new ClusterPropertyLoader());
     }
 
     public EntityLoader getLoader(String type) {
