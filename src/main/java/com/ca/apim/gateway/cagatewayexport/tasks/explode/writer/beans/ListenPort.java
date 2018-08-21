7a79d2ba8b5ae726919e9e3818bd19d2b1a6319a
Borges Caldeira, Joao Gabriel <JoaoGabriel.BorgesCaldeira@ca.com>
2018-08-21 10:32:35 -0700

    Adding copyright to new files

--- src/main/java/com/ca/apim/gateway/cagatewayexport/tasks/explode/writer/beans/ListenPort.java
+++ src/main/java/com/ca/apim/gateway/cagatewayexport/tasks/explode/writer/beans/ListenPort.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2018 CA. All rights reserved.
+ * This software may be modified and distributed under the terms
+ * of the MIT license.  See the LICENSE file for details.
+ */
+
 package com.ca.apim.gateway.cagatewayexport.tasks.explode.writer.beans;
 
 import java.util.List;
