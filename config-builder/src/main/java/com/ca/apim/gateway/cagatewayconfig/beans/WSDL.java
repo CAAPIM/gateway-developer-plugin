25cdec959869ffddba5259edee6b75820c698cd1
Agrawal, Amogh <Amogh.Agrawal@ca.com>
2018-11-14 16:14:37 -0800

    126 - Fixing issue of not being able to export soap services from gateway. Removing duplicate methods.

---
+++ config-builder/src/main/java/com/ca/apim/gateway/cagatewayconfig/beans/WSDL.java
@@ -0,0 +1,23 @@
+package com.ca.apim.gateway.cagatewayconfig.beans;
+
+public class WSDL {
+
+    private String url;
+    private String wsdl;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getWsdl() {
+        return wsdl;
+    }
+
+    public void setWsdl(String wsdl) {
+        this.wsdl = wsdl;
+    }
+}
