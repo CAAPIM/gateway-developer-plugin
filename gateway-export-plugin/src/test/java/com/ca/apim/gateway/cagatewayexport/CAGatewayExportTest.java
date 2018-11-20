b52155b6d9e4a9df3546fffd35f2ddf66d799132
Johnny Lou <Johnny.lou8@gmail.com>
2018-11-20 10:20:21 -0800

    Bugfix/110 Export failure when non-existent folder path is specified (#131)
    * 110 - Add validation of imports by comparing specified folder in config and imported restman bundle
    
    * 110 - Corrected boolean values
    
    * 110 - Rename method
    
    * 110 - Reimplement verification method
    
    * 110 - Revert changes to BundleFilter.java
    
    * 110: Address code changes
    
    * 110 - Address sonar issues
    
    * 110 - Move method to ExplodeBundle.java & add a unit test
    
    * 110 - Remove extra new line
    
    * 110 Address code changes
    
    * 110 - Simplify method
    
    * 110 - Changed exception message

--- gateway-export-plugin/src/test/java/com/ca/apim/gateway/cagatewayexport/CAGatewayExportTest.java
+++ gateway-export-plugin/src/test/java/com/ca/apim/gateway/cagatewayexport/CAGatewayExportTest.java
@@ -176,4 +176,45 @@ class CAGatewayExportTest {
         assertTrue(exception.getMessage().contains("unknown-password"));
         assertTrue(exception.getMessage().contains("Missing"));
     }
+
+    @Test
+    @ExtendWith(TemporaryFolderExtension.class)
+    void testExplodeMismatchFolderPath(TemporaryFolder temporaryFolder) throws IOException, URISyntaxException {
+        String projectFolder = "example-project";
+        File testProjectDir = new File(temporaryFolder.getRoot(), projectFolder);
+        File buildGradleFile = new File(testProjectDir, "build.gradle");
+        File bundleFile = new File(testProjectDir, "bundle.bundle");
+
+        String gradleBuild = "" +
+                "plugins {\n" +
+                "    id 'com.ca.apim.gateway.gateway-export-plugin-base'\n" +
+                "}\n" +
+                "group 'com.ca'\n" +
+                "version '1.2.3-SNAPSHOT'\n" +
+                "task('explode', type: com.ca.apim.gateway.cagatewayexport.tasks.explode.ExplodeBundleTask) {\n" +
+                "    folderPath = '/non-existing-folder'\n" +
+                "    inputBundleFile = file('bundle.bundle')\n" +
+                "    exportDir = file('gateway')\n" +
+                "    exportEntities = [\n" +
+                "        passwords: [ \"my-password\" ],\n" +
+                "        unknownEntities: [ \"some-other-entity\" ],\n" +
+                "        clusterProperties: [ \"that-property\" ]\n" +
+                "    ]\n" +
+                "}";
+
+        FileUtils.writeStringToFile(buildGradleFile, gradleBuild, Charset.defaultCharset());
+
+        String bundle = FileUtils.readFileToString(new File(Objects.requireNonNull(getClass().getClassLoader().getResource("bundles/environment-properties-test.bundle")).toURI()), Charset.defaultCharset());
+        FileUtils.writeStringToFile(bundleFile, bundle, Charset.defaultCharset());
+
+        UnexpectedBuildFailure exception = assertThrows(UnexpectedBuildFailure.class, () -> GradleRunner.create()
+                .withProjectDir(testProjectDir)
+                .withArguments("explode", "--stacktrace")
+                .withPluginClasspath()
+                .withDebug(true)
+                .build());
+
+        assertTrue(exception.getMessage().contains("does not exist in the target gateway."));
+    }
+
 }
