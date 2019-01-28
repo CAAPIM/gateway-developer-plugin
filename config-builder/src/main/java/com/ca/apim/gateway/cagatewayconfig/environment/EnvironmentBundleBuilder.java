b63bfc52febb20e5459d9fbb6611ac61842ab764
Borges Caldeira, Joao Gabriel <JoaoGabriel.BorgesCaldeira@ca.com>
2019-01-28 14:59:46 -0800

    allowing environment provided as files on docker-compose files

--- config-builder/src/main/java/com/ca/apim/gateway/cagatewayconfig/environment/EnvironmentBundleBuilder.java
+++ config-builder/src/main/java/com/ca/apim/gateway/cagatewayconfig/environment/EnvironmentBundleBuilder.java
@@ -9,19 +9,27 @@ package com.ca.apim.gateway.cagatewayconfig.environment;
 import com.ca.apim.gateway.cagatewayconfig.beans.Bundle;
 import com.ca.apim.gateway.cagatewayconfig.config.loader.EntityLoader;
 import com.ca.apim.gateway.cagatewayconfig.config.loader.EntityLoaderRegistry;
+import com.ca.apim.gateway.cagatewayconfig.util.environment.EnvironmentConfigurationUtils;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.io.File;
 import java.util.Map;
 
 @Singleton
 public class EnvironmentBundleBuilder {
 
+    private static final String ENVIRONMENT_FILES_CONFIG_PATH = "/opt/SecureSpan/Gateway/node/default/etc/bootstrap/env";
+    private static final String ENV_PREFIX = "ENV.";
+    private static final String FILE_PREFIX = "FILE.";
+
     private final EntityLoaderRegistry entityLoaderRegistry;
+    private final EnvironmentConfigurationUtils environmentConfigurationUtils;
 
     @Inject
-    EnvironmentBundleBuilder(EntityLoaderRegistry entityLoaderRegistry) {
+    EnvironmentBundleBuilder(EntityLoaderRegistry entityLoaderRegistry, EnvironmentConfigurationUtils environmentConfigurationUtils) {
         this.entityLoaderRegistry = entityLoaderRegistry;
+        this.environmentConfigurationUtils = environmentConfigurationUtils;
     }
 
     void build(Bundle bundle, Map<String, String> environmentProperties) {
@@ -29,17 +37,35 @@ public class EnvironmentBundleBuilder {
     }
 
     private void addEnvToBundle(Bundle bundle, String key, String value) {
-        if (key.startsWith("ENV.")) {
-            String environmentKey = key.substring(4);
-            int typeEndIndex = environmentKey.indexOf('.');
-            if(typeEndIndex != -1) {
-                String type = environmentKey.substring(0, typeEndIndex);
-                EntityLoader loader = entityLoaderRegistry.getLoader(type);
-                if(loader != null) {
-                    String name = environmentKey.substring(type.length() + 1);
-                    loader.load(bundle, name, value);
-                }
-            }
+        if (!key.startsWith(ENV_PREFIX)) {
+            return;
         }
+
+        String environmentKey = key.substring(4);
+        // check if var is starting with file prefix - then remove if true
+        boolean isFile = environmentKey.startsWith(FILE_PREFIX);
+        if (isFile) {
+            environmentKey = environmentKey.substring(5);
+        }
+
+        int typeEndIndex = environmentKey.indexOf('.');
+        if (typeEndIndex == -1) {
+            return;
+        }
+
+        String type = environmentKey.substring(0, typeEndIndex);
+        EntityLoader loader = entityLoaderRegistry.getLoader(type);
+        if (loader == null) {
+            return;
+        }
+
+        String name = environmentKey.substring(type.length() + 1);
+        if (isFile) {
+            // if we have a file, replace the value contents with the content load from the file in JSON specification.
+            value = environmentConfigurationUtils.loadConfigFromFile(new File(ENVIRONMENT_FILES_CONFIG_PATH, value), type, name);
+        }
+
+        // then load it
+        loader.load(bundle, name, value);
     }
 }
