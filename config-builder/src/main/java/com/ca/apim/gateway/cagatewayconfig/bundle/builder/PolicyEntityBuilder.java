3f308bf874a633932990a201f009047b55478c1f
Srinivas Uppoju <su671781@broadcom.net>
2020-06-24 09:51:44 +0100

    Unique name space is added to code entities

--- config-builder/src/main/java/com/ca/apim/gateway/cagatewayconfig/bundle/builder/PolicyEntityBuilder.java
+++ config-builder/src/main/java/com/ca/apim/gateway/cagatewayconfig/bundle/builder/PolicyEntityBuilder.java
@@ -156,7 +156,7 @@ public class PolicyEntityBuilder implements EntityBuilder {
             if (annotatedEntity.isReusable() || isAnnotatedEntity(policy, annotatedEntity)) {
                 policyName = policy.getName();
             } else {
-                policyName = annotatedBundle.getUniquePrefix() + policy.getName();
+                policyName = annotatedBundle.applyUniqueName(policy.getName());
             }
         } else {
             policyName = policy.getName();
@@ -372,7 +372,7 @@ public class PolicyEntityBuilder implements EntityBuilder {
                 encassGuid = idGenerator.generateGuid();
                 encass.setGuid(encassGuid);
                 encass.setId(idGenerator.generate());
-                encassName = annotatedBundle.getUniquePrefix() + encassName;
+                encassName = annotatedBundle.applyUniqueName(encassName);
             }
         }
         Element encapsulatedAssertionConfigNameElement = createElementWithAttribute(
@@ -541,7 +541,7 @@ public class PolicyEntityBuilder implements EntityBuilder {
         }
         if (annotatedBundle != null) {
             if (!isReusable && !isAnnotatedEntity(policy, annotatedEntity)) {
-                policyName = annotatedBundle.getUniquePrefix() + policyName;
+                policyName = annotatedBundle.applyUniqueName(policyName);
                 policyNameWithPath = PathUtils.extractPath(policyNameWithPath) + policyName;
             }
         }
