df02216e82d1377cc601ee806490a063fac509e6
Raju Gurram <raju.gurram@broadcom.com>
2020-08-05 15:07:57 +0530

    unsupported-entity of type SERVER_MODULE_FILE is chosen to be excluded by default

--- gateway-export-plugin/src/main/java/com/ca/apim/gateway/cagatewayexport/tasks/explode/bundle/BundleBuilder.java
+++ gateway-export-plugin/src/main/java/com/ca/apim/gateway/cagatewayexport/tasks/explode/bundle/BundleBuilder.java
@@ -128,6 +128,10 @@ public class BundleBuilder {
         entity.setName(getSingleChildElement(element, NAME).getTextContent());
         entity.setElement(element);
 
+        if ("SERVER_MODULE_FILE".equals(entity.getType())) {
+            entity.setExcluded(true);
+        }
+
         LOGGER.log(Level.WARNING, "Recording the {0}/{1} with id: {2} as unsupported entity",
                 new Object[] {entity.getType(), entity.getName(), entity.getId()});
         bundle.addEntity(entity);
